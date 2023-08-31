f986bc7f15a381dab5ce0c6c5c2d3885d9ee46d0
Henry Peeples <hgpeeples48@gmail.com>
2023-08-31 18:52:41 -0500

    add file uploading and image preview

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,7 +1,48 @@
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const FileUploader = () => {
+  const [file, setFile] = useState<string | null>(null);
+
+  const onDrop = useCallback((acceptedFiles) => {
+    const reader = new FileReader();
+
+    reader.onabort = () => toast('file reading was aborted');
+    reader.onerror = () => toast('file reading has failed');
+    reader.onload = () => {
+      const dataURL: string | null = typeof reader.result === 'string' ? reader.result : null;
+      if (dataURL !== null) {
+        setFile(dataURL);
+        toast('File uploaded successfully...');
+      } else toast('unable to load file');
+    };
+    reader.readAsDataURL(acceptedFiles[0]);
+  }, []);
+
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
   return (
-    <div className="flex flex-col bg-gray-200 rounded-md h-[49%] sm:h-full w-full sm:w-[49%] border-dashed border-[1px] border-gray-400 justify-center">
-      <p className="self-center">Click to choose or drag and drop file here...</p>
+    <div className="flex flex-col h-[49%] sm:h-full w-full sm:w-[49%] justify-center">
+      <div
+        className="flex items-center justify-center w-full h-full border-[1px] border-gray-400 border-dashed 
+          rounded-md hover:bg-gray-200 transition-all duration-150 ease-in-out cursor-pointer"
+        {...getRootProps()}
+      >
+        <input {...getInputProps()} />
+        {file ? (
+          <img src={file} className="block max-w-[95%] max-h-[95%] w-auto h-auto" />
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+        <ToastContainer
+          position="bottom-right"
+          autoClose={4000}
+          draggable={false}
+          closeOnClick={false}
+        />
+      </div>
     </div>
   );
 };
