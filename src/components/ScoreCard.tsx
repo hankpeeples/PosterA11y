3505b491cb8efceac42f5b431b685feea264bb23
Henry Peeples <hgpeeples48@gmail.com>
2023-09-06 17:21:27 -0500

    update score card styling

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,7 +1,7 @@
 const ScoreCard = () => {
   return (
-    <div className="flex flex-col bg-gray-800 rounded-md h-[49%] sm:h-full w-full sm:w-[49%] justify-center">
-      <p className="self-center text-white">Score Card</p>
+    <div className="flex flex-col border-solid border-[1px] border-black rounded-md h-[49%] sm:h-full w-full sm:w-[49%] justify-center">
+      <p className="self-center text-black">Score Card</p>
     </div>
   );
 };
