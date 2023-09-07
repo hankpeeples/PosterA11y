02a336525d27883f32dbea2c77b502ad387a01e7
Henry Peeples <hgpeeples48@gmail.com>
2023-09-07 16:04:00 -0500

    finish image uploader

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -4,46 +4,63 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 const FileUploader = () => {
-  const [file, setFile] = useState<string | null>(null);
+  type UploadedFile = {
+    fileName: string | null;
+    imageData: string;
+  };
 
-  const onDrop = useCallback((acceptedFiles) => {
-    const reader = new FileReader();
+  const [file, setFile] = useState<UploadedFile>({ fileName: null, imageData: '' });
 
-    reader.onabort = () => toast.error('File reading was aborted.');
-    reader.onerror = () => toast.error('File reading has failed.');
-    reader.onload = () => {
-      const dataURL: string | null = typeof reader.result === 'string' ? reader.result : null;
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const reader = new FileReader();
 
-      if (dataURL !== null) {
-        setFile(dataURL);
-        toast.success('File uploaded successfully...');
-      } else {
-        toast.error('Unable to load file.');
-      }
-    };
-    reader.readAsDataURL(acceptedFiles[0]);
-  }, []);
+      reader.onabort = () => toast.error('File reading was aborted.');
+      reader.onerror = () => toast.error('File reading has failed.');
+      reader.onload = () => {
+        const dataURL: string | null = typeof reader.result === 'string' ? reader.result : null;
+
+        if (dataURL !== null) {
+          setFile({ fileName: acceptedFiles[0].name, imageData: dataURL });
+          toast.success('File uploaded successfully! Starting analysis...');
+        } else {
+          toast.error('Unable to load file.');
+        }
+      };
+
+      reader.readAsDataURL(acceptedFiles[0]);
+    },
+    [file]
+  );
 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
     <div className="flex flex-col h-[49%] sm:h-full w-full sm:w-[49%] justify-center">
       <div
-        className="flex items-center justify-center w-full h-full border-[1px] border-gray-400 border-dashed 
+        className="flex w-full h-full items-center justify-center border-[1px] border-gray-400 border-dashed 
           rounded-md hover:bg-gray-200 transition-all duration-150 ease-in-out cursor-pointer"
         {...getRootProps()}
       >
         <input {...getInputProps()} />
-        {file ? (
-          <img src={file} className="block max-w-[95%] max-h-[95%] w-auto h-auto" />
+        {file.imageData !== '' ? (
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <img src={file.imageData} className="block max-w-[90%] max-h-[90%] w-auto h-auto" />
+            <p className="text-sm text-gray-400">{file.fileName}</p>
+          </div>
         ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p className="font-semibold text-gray-400">
+            Drag 'n' drop a file here, or click to select file
+          </p>
         )}
         <ToastContainer
           position="bottom-right"
           autoClose={4000}
           draggable={false}
           closeOnClick={false}
+          pauseOnFocusLoss={false}
+          pauseOnHover={true}
+          // theme="colored"
         />
       </div>
     </div>
