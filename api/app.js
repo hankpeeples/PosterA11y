57365cc79644247e95ad1115b37f4793ebf405b3
Henry Peeples <hgpeeples48@gmail.com>
2023-09-18 18:38:05 -0500

    fix: payload too large request error

--- api/app.js
+++ api/app.js
@@ -1,16 +1,17 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(cors())
+app.use(express.json({ limit: '10mb', extended: true }));
+app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 50000 }));
+app.use(cors());
 
-app.get('/api/v1/processPoster', (req, res) => {
-  console.log(req.body)
+app.post('/api/v1/processPoster', (req, res) => {
+  console.log(req.body);
   res.json({
     data: 'test',
   });
