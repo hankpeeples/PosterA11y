2da330c606794ecef6c9f2fd7760f546d8158e97
Henry Peeples <hgpeeples48@gmail.com>
2023-09-18 18:38:35 -0500

    update request to include image data

--- src/utils/fileOptimizer.ts
+++ src/utils/fileOptimizer.ts
@@ -3,9 +3,15 @@
 export const optimizer = async (image: string): Promise<string> => {
   return new Promise(async (resolve, reject) => {
     try {
-      let res = await fetch('http://localhost:3001/api/v1/processPoster');
-      console.log(res);
-      resolve(image);
+      let res = await fetch('http://localhost:3001/api/v1/processPoster', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ image }),
+      });
+      const newImage = await res.json();
+      resolve(newImage);
     } catch (err) {
       reject(err);
     }
