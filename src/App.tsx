51b48334b2635065d7afb5b60a174e89354ca39a
Henry Peeples <hgpeeples48@gmail.com>
2023-09-18 18:11:10 -0500

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import ExplanationCard from './components/ExplanationCard';
 import FileUploader from './components/FileUploader';
 import Navbar from './components/Navbar';
@@ -5,9 +6,9 @@ import ScoreCard from './components/ScoreCard';
 
 function App() {
   return (
-    <main className="flex flex-col h-screen w-screen bg-[#f5f5f5] text-black">
+    <main className="flex h-screen w-screen flex-col bg-[#f5f5f5] text-black">
       <Navbar />
-      <div className="flex flex-col justify-between p-4 m-4 sm:flex-row h-3/5 sm:h-2/5">
+      <div className="flex flex-col justify-between p-4 m-4 h-3/5 sm:h-2/5 sm:flex-row">
         <FileUploader />
         <ScoreCard />
       </div>
