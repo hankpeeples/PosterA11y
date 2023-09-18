4b44fbf55ea06867933232ff17fc6a5cc14ab16d
Henry Peeples <hgpeeples48@gmail.com>
2023-09-18 18:11:03 -0500

    add basic fetch api request

--- src/utils/fileOptimizer.ts
+++ src/utils/fileOptimizer.ts
@@ -1,9 +1,11 @@
-import { Image } from 'image-js';
+// import { Image } from 'image-js';
 
 export const optimizer = async (image: string): Promise<string> => {
   return new Promise(async (resolve, reject) => {
     try {
-      resolve('');
+      let res = await fetch('http://localhost:3001/api/v1/processPoster');
+      console.log(res);
+      resolve(image);
     } catch (err) {
       reject(err);
     }
