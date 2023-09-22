c50540af61b12780dddf8f555f4b1e6d386292bf
Henry Peeples <hgpeeples48@gmail.com>
2023-09-21 19:36:55 -0500

    fix: make fs saving async to make sure file is there

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import Jimp from 'jimp';
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 
 dotenv.config();
 
@@ -14,12 +14,13 @@ app.use(cors());
 
 app.post('/api/v1/optimize', async (req, res) => {
   const regex = new RegExp(`^data:.*?;base64,`, 'gi');
+  // make new buffer from file url string
   const imgBuffer = new Buffer.from(req.body.image.replace(regex, ''), 'base64url');
   const filePath = './' + req.body.fileName;
-  fs.writeFile(filePath, imgBuffer, (err) => {
-    if (err) res.json({ error: err });
-    console.log(`Saved ${filePath}`);
-  });
+  // save buffer/file to fs
+  await fs.writeFile(filePath, imgBuffer);
+  console.log(`Saved ${filePath}`);
+  // run optimizations
   const newImage = await optimizeIncomingImage(filePath);
   if (newImage.error) {
     res.json({ error: newImage.error });
@@ -37,7 +38,10 @@ app.listen(port, () => console.log(`API running on :${port}`));
 const optimizeIncomingImage = async (path) => {
   try {
     const jimpImg = await Jimp.read(path);
-    console.log(jimpImg.getHeight());
+    // image has been read from file system, can delete it now
+    await fs.unlink(path);
+    console.log(`Removed ${path}`);
+    console.log('Image height:', jimpImg.getHeight());
     return await jimpImg.getBase64Async(Jimp.AUTO);
   } catch (err) {
     console.log('optimizeIncomingImage():', err.message);
