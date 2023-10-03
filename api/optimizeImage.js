099269a192d0e25f69909f0f895dad3c8dceadc7
Henry Peeples <hgpeeples48@gmail.com>
2023-10-03 15:22:25 -0500

    more img manip adjustments and add getText() call

--- api/optimizeImage.js
+++ api/optimizeImage.js
@@ -1,6 +1,6 @@
-import util from 'node:util';
-import { exec } from 'node:child_process';
+import { execSync } from 'node:child_process';
 import fs from 'node:fs/promises';
+import getText from './getText.js';
 
 /*
  * OptimizeImage will adjust the following image properties:
@@ -12,41 +12,58 @@ import fs from 'node:fs/promises';
  *
  *    'convert' is using the ImageMagic command line tool
  */
-const execProm = util.promisify(exec);
+
+const boxColors = ['red', 'blue', 'green', 'orange', 'yellow', 'purple'];
 
 const optimizeImage = async (path) => {
   try {
-    // add 10px white border
-    await execProm(`convert ${path} -bordercolor White -border 10x10 ${path}`);
     // set dpi to 600
-    await execProm(`convert ${path} -units PixelsPerInch -density 600 ${path}`);
-    // remove image alpha
-    await execProm(`convert ${path} -alpha off ${path}`);
+    execSync(`convert ${path} -units PixelsPerInch -density 600 ${path}`);
     // convert to greyscale and sharpen
-    await execProm(
-      `convert ${path} -set colorspace Gray -separate -average -sharpen 0x3.0 ${path}`
+    execSync(
+      `convert ${path} -alpha off -colorspace gray -type grayscale -contrast-stretch 0 -negate \
+          -contrast-stretch 1 -compose copy_opacity -opaque none -sharpen 0x1 ${path}`
     );
     // remove noise
-    await execProm(
-      `convert ${path} -fuzz 10% -fill Black -opaque Black -define quantum:format=floating-point -depth 32 ${path}`
+    execSync(
+      `convert ${path} -fuzz 10% -fill Black -define quantum:format=floating-point -depth 32 \
+        -enhance -enhance ${path}`
     );
-    // chain enhance
-    await execProm(
-      `convert ${path} -enhance -enhance -enhance -enhance -enhance -enhance -enhance -enhance ${path}`
-    );
+    // add 10px white border
+    execSync(`convert ${path} -bordercolor White -border 10x10 ${path}`);
 
-    const newImg = await fs.readFile(path, 'base64');
-    const ext = path.slice(path.lastIndexOf('.'));
+    const optImage = await readFile(path);
+    // doing getText here so I can draw on it after
+    const { text, bbox } = await getText(optImage);
+
+    const clen = boxColors.length - 1;
+    let i = 0;
+
+    for (let box of bbox) {
+      execSync(
+        `convert ${path} -fill none -stroke ${boxColors[i]} -strokewidth 2 -draw "rectangle ${box.x0},${box.y0} ${box.x1},${box.y1}" ${path}`
+      );
+      i++;
+      if (i > clen) i = 0;
+    }
+
+    const newImage = await readFile(path);
 
     // image has been read from file system, can delete it now
     await fs.unlink(path);
     console.log(`Removed ${path}`);
 
-    return `data:image/${ext};base64,${newImg}`;
+    return newImage;
   } catch (err) {
     console.error('optimizeIncomingImage():', err.message);
     return { error: 'optimizeIncomingImage(): ' + err.message };
   }
+};
+
+const readFile = async (path) => {
+  const newImg = await fs.readFile(path, 'base64');
+  const ext = path.slice(path.lastIndexOf('.') + 1);
+  return `data:image/${ext};base64,${newImg}`;
 };
 
 export default optimizeImage;
