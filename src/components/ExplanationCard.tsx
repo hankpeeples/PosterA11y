2baf06bd176b9d53dcd34fdca0cc3dd7350eecc4
Henry Peeples <hgpeeples48@gmail.com>
2023-10-18 16:51:34 -0500

    update explanation header

--- src/components/ExplanationCard.tsx
+++ src/components/ExplanationCard.tsx
@@ -1,7 +1,7 @@
 const ExplanationCard = () => {
   return (
-    <div className="flex h-full w-full self-center rounded-md bg-gray-300 shadow-md shadow-[#898989]">
-      ExplanationCard
+    <div className="flex h-full w-full rounded-md bg-gray-300 p-4 shadow-md shadow-[#898989]">
+      <h2 className="font-bold text-black text-[1.5rem]">Noteable Info</h2>
     </div>
   );
 };
