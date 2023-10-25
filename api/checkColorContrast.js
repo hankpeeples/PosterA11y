8dc9212c33061307982bda01079b45338dfd7b56
Henry Peeples <hgpeeples48@gmail.com>
2023-10-24 21:44:28 -0500

    get proper contrast score

--- api/checkColorContrast.js
+++ api/checkColorContrast.js
@@ -4,47 +4,52 @@ import ColorContrastChecker from 'color-contrast-checker';
 import { execSync } from 'node:child_process';
 
 const checkContrast = async (path) => {
-  const ccc = new ColorContrastChecker();
+  return new Promise(async (resolve, reject) => {
+    const ccc = new ColorContrastChecker();
 
-  try {
-    let colors = [];
-    getPixels('colored-' + path, async (err, pixels) => {
-      if (!err) {
-        const data = [...pixels.data];
-        const width = Math.round(Math.sqrt(data.length / 4));
-        const height = width;
+    try {
+      let colors = [];
+      getPixels('colored-' + path, async (err, pixels) => {
+        if (!err) {
+          const data = [...pixels.data];
+          const width = Math.round(Math.sqrt(data.length / 4));
+          const height = width;
 
-        const cExt = await extractColors({ data, width, height });
-        cExt.forEach((color) => {
-          colors.push(color.hex);
-        });
+          try {
+            const cExt = await extractColors({ data, width, height });
+            cExt.forEach((color) => {
+              colors.push(color.hex);
+            });
 
-        const cLen = colors.length - 1;
-        let pass = 0;
+            const cLen = colors.length - 1;
+            let pass = 0;
 
-        // check contrast, each color will be checked with each color except itself
-        for (let i = 0; i < cLen; i++) {
-          for (let k = 0; k !== cLen + 1; k++) {
-            if (k !== i) {
-              // minimum acceptable WCAG ratio is 4.5:1
-              if (ccc.isLevelCustom(colors[i], colors[k], 4.5)) {
-                pass++;
+            // check contrast, each color will be checked with each color except itself
+            for (let i = 0; i < cLen; i++) {
+              for (let k = 0; k !== cLen + 1; k++) {
+                if (k !== i) {
+                  // minimum acceptable WCAG ratio is 4.5:1
+                  if (ccc.isLevelCustom(colors[i], colors[k], 4.5)) {
+                    pass++;
+                  }
+                }
               }
             }
+
+            await generateColorPalette(colors);
+
+            resolve((pass / (cLen * 2 - 1)) * 100);
+          } catch (err) {
+            console.log('color contrast inner fun:', err);
+            reject(err);
           }
         }
-
-        await generateColorPalette(colors);
-
-        if (pass >= cLen * 2 - 1) {
-          return true;
-        }
-        return false;
-      }
-    });
-  } catch (err) {
-    console.log('color contrast err:', err);
-  }
+      });
+    } catch (err) {
+      console.log('color contrast err:', err);
+      reject(err);
+    }
+  });
 };
 
 const generateColorPalette = async (colors) => {
