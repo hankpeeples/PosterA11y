6fa22e95e4da75eb22f83b9a5e5f9821b06305db
Henry Peeples <hgpeeples48@gmail.com>
2023-11-07 13:41:36 -0600

    update file extensions

--- api/app.js
+++ api/app.js
@@ -21,9 +21,17 @@ app.use(
 
 app.post('/api/v1/analyze', async (req, res) => {
   const regex = new RegExp(`^data:.*?;base64,`, 'gi');
+  let ext = req.body.image.match(regex);
+  ext = '.' + ext[0].substring(ext[0].indexOf('/') + 1, ext[0].indexOf(';'));
+
   // make new buffer from file url string
   const imgBuffer = new Buffer.from(req.body.image.replace(regex, ''), 'base64url');
-  const filePath = req.body.fileName;
+  let filePath = req.body.fileName;
+
+  if (!filePath.includes('.')) filePath += ext;
+
+  console.log(filePath);
+
   // save buffer/file to fs
   await fs.writeFile(filePath, imgBuffer);
   await fs.copyFile(filePath, 'colored-' + filePath);
